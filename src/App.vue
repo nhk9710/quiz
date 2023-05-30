0d7b0a48710122ab27315f8bfc4f71a5e068a9e9
nohokwan <nhk9710@naver.com>
2023-05-30 15:19:04 +0900

    [feat]메인페이지 생성

--- src/App.vue
+++ src/App.vue
@@ -6,48 +6,82 @@ export default {
   setup(){
     const quizData = ref(data.trivia_categories);
     const selectData = ref('');
+    const numberOfQuestion = ref(1);
+    const difficulty = ref(['easy','medium','hard']);
+    const selectDifficulty = ref('easy');
+    const quizType = ref(['True&False','multiple','any'])
+    const selectType = ref('any');
 
     return{
       quizData,
-      selectData
+      selectData,
+      numberOfQuestion,
+      difficulty,
+      selectDifficulty,
+      quizType,
+      selectType
     }
   }
 }
 </script>
 
 <template>
-  <div>
+  <div class="wrapBox">
+    <div class="innerBox">
+      <div style="display: flex; width: 100%; justify-content: center; margin-bottom: 2%;">
+        <span style="color: white; font-size: 2.5em">퀴즈</span>
+      </div>
+    <div class="flex">
+      <span>퀴즈 종류 : </span>
     <select v-model="selectData">
       <option :value="v.name" v-for="(v,i) in quizData">{{ v.name }}</option>
     </select>
+    </div>
+    <div class="flex">
+      <span>퀴즈 숫자 : </span>
+    <input type="number" v-model="numberOfQuestion" min="1" max="50">
+    </div>
+    <div class="flex">
+      <span>퀴즈 난이도 : </span>
+    <select v-model="selectDifficulty">
+      <option :value="v" v-for="(v,i) in difficulty">{{ v }}</option>
+    </select>
+    </div>
+      <div class="flex">
+        <span>퀴즈 타입 : </span>
+        <select v-model="selectType">
+          <option :value="v" v-for="(v,i) in quizType">{{ v }}</option>
+        </select>
+      </div>
+      <div style="display: flex; width: 100%; justify-content: center; margin-bottom: 2%;">
+        <button type="button" class="btn btn-primary" style="font-size: 2.5em">시작!</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+.innerBox{
+  width: 35%;
+  height: 50%;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+.wrapBox{
+  background: #24252f;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+.flex{
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 1%;
+  color: white;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
